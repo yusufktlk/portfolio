7e91099b85b387cd129103fd323fb4481ed6d98a
Yusuf Kıtlık <yusufbjk__44@hotmail.com>
2023-08-22 16:22:27 +0300

    responsiveFix

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,13 +5,13 @@ function Tech() {
     <div>
         <h1 id='tech' className='text-5xl ml-4 md:ml-44 mb-16 md:mb-24 mt-16 md:mt-24 font-thin'>TECH STACK</h1>
         <div className='flex md:flex md:justify-center gap-x-3 ml-4 md:ml-0 md:gap-x-12 mb-20 md:mb-32'>
-            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[100px]' src='/HTML5.png' />
-            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[100px]' src='/CSS.png' />
-            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[100px]' src='/TailwindCSS.png' />
-            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[100px]' src='/Javascript.png' />
-            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[100px]' src='/React.png' />
-            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[100px]' src='/Figma.png' />
-            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[100px]' src='/Github.png' />
+            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[70px]' src='/HTML5.png' />
+            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[70px]' src='/CSS.png' />
+            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[70px]' src='/TailwindCSS.png' />
+            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[70px]' src='/Javascript.png' />
+            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[70px]' src='/React.png' />
+            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[70px]' src='/Figma.png' />
+            <img className='hover:-translate-y-5 duration-500 h-[40px] md:h-[70px] w-[40px] md:w-[70px]' src='/Github.png' />
         </div>
     </div>
   )
