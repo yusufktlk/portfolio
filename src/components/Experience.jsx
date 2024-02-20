88c03cf84007c4ee48d62f7d27d4763911e20339
Yusuf <yusufbjk__44@hotmail.com>
2024-02-20 10:25:53 +0300

    new project added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,14 +4,27 @@ function Experience() {
   return (
     <div className='flex flex-col mx-4 md:mx-44 mt-52 my-16 md:my-24 tracking-widest font-thin ' id='resume'>
       <h1 className='text-5xl font-thin'>EXPERIENCE</h1>
+      <div>
       <div className='flex md:mr-12 mt-12 items-center justify-between'>
-          <h1 className='text-lg md:text-3xl'>Frontend Developer (Intern)</h1>
-          <h3 className='text-sm md:text-base' >July 2023 - August 2023</h3>
+          <h1 className='text-lg md:text-3xl'>Fullstack Developer (Intern)</h1>
+          <h3 className='text-sm md:text-base' >Feb 2024 - Current</h3>
       </div>
-      <h3 className='text-purple-600  mt-8'>Klonbits Information Solutions Inc. / TR - Antalya</h3>
+      <h3 className='text-purple-600  mt-8'>Softalya Software Inc. / TR - Antalya</h3>
       <ul className='mt-8 font-thin'>
-        <li>- Since I have a compulsory internship in the 3rd year of the university, I completed the summer of 2023 by doing an internship.</li>
+        <li>- I did my long-term compulsory internship here in the last year of my university. </li>
       </ul>
+      </div>
+      
+      <div>
+        <div className='flex md:mr-12 mt-12 items-center justify-between'>
+            <h1 className='text-lg md:text-3xl'>Frontend Developer (Intern)</h1>
+            <h3 className='text-sm md:text-base' >July 2023 - August 2023</h3>
+        </div>
+        <h3 className='text-purple-600  mt-8'>Klonbits Information Solutions Inc. / TR - Antalya</h3>
+        <ul className='mt-8 font-thin'>
+          <li>- Since I have a compulsory internship in the 3rd year of the university, I completed the summer of 2023 by doing an internship.</li>
+        </ul>
+      </div>
     </div>
   )
 }
